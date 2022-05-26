0363366ec813d53fb820310ffe4e1ff9993af21c
Racha777 <jramoschang@gmail.com>
2022-05-25 23:03:37 -0500

    Link y NavLink

--- src/layouts/Pokemons/PokemonsLayout.jsx
+++ src/layouts/Pokemons/PokemonsLayout.jsx
@@ -2,10 +2,15 @@ import { Outlet } from "react-router-dom";
 import Header from "../../component/sections/Header";
 
 const PokemonsLayout = () => {
+
+  const applicationName="PokemonInc";
+
   return (
     <>
-      <Header/>
-      <main className="text-center">
+      <Header
+        applicationName={applicationName}
+      />
+      <main className="text-center pt-16 pb-8">
         <Outlet></Outlet>
       </main>
       <footer className="mt-auto text-center">Racha</footer>
