a9f84c75e0a1458f05762de0afee1c9809468cb4
Racha777 <jramoschang@gmail.com>
2022-05-26 22:11:51 -0500

    Component PokemonsForm

--- src/component/pokemons/PokemonsForm.jsx
+++ src/component/pokemons/PokemonsForm.jsx
@@ -1,35 +1,65 @@
-import { Field, Form,Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 
 const PokemonsForm = () => {
   return (
-        <Formik 
-            initialValues={{
-                name:'',
-                type:'',
-                hp:'',
-                attack:'',
-                special:'',
-                image:{}
-            }}
-            onSubmit={(values)=>{
-                console.log(values);
-            }}
-        >
-            {({handleSubmit})=>{
-                return(
-                    <Form
-                        onSubmit={handleSubmit}
-                    >
-                        <Field name='name' placeholder='Nombre' className="bg-zinc-800 rounded p-2"/>
-                        <Field name='type' placeholder='Tipo' className="bg-zinc-800 rounded p-2"/>
-                        <Field name='hp' placeholder='Hp' className="bg-zinc-800 rounded p-2"/>
-                        <Field name='attack' placeholder='Ataque' className="bg-zinc-800 rounded p-2"/>
-                        <Field name='special' placeholder='Especial' className="bg-zinc-800 rounded p-2"/>
-                        <button type='submit'>Enviar</button>
-                    </Form>
-                );
-            }}
-        </Formik>
+    <Formik
+      initialValues={{
+        name: "",
+        type: "",
+        hp: "",
+        attack: "",
+        special: "",
+        image: {},
+      }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ handleSubmit }) => {
+        return (
+          <Form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-4 bg-zinc-700 rounded-md w-4/5 max-w-sm mx-auto p-4"
+          >
+            <Field
+              name="name"
+              type="text"
+              placeholder="Nombre"
+              className="bg-zinc-800 rounded p-2"
+            />
+            <Field
+              name="type"
+              type="text"
+              placeholder="Tipo"
+              className="bg-zinc-800 rounded p-2"
+            />
+            <Field
+              name="hp"
+              type="number"
+              placeholder="Hp"
+              className="bg-zinc-800 rounded p-2"
+            />
+            <Field
+              name="attack"
+              type="text"
+              placeholder="Ataque"
+              className="bg-zinc-800 rounded p-2"
+            />
+            <Field
+              name="special"
+              type="text"
+              placeholder="Especial"
+              className="bg-zinc-800 rounded p-2"
+            />
+            <input
+              type="file"
+              className="bg-zinc-800 rounded p-2 text-sm cursor-pointer file:bg-white file:border-0 file:rounded file:font-semibold file:mr-2 file:p-1 file:px-2 file:cursor-pointer file:transition-colors hover:file:bg-zinc-200"
+            />
+            <button type="submit" className="bg-cyan-300 rounded-md text-black font-semibold p-2 transition-colors hover:bg-white">Enviar</button>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 };
 
