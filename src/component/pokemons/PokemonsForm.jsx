e7df2e8901da7d2a853b21d9ac4bb24e731fa04e
Racha777 <jramoschang@gmail.com>
2022-05-26 21:18:15 -0500

    Uso Formulario con Formik

--- src/component/pokemons/PokemonsForm.jsx
+++ src/component/pokemons/PokemonsForm.jsx
@@ -1,7 +1,36 @@
-const PokemonsForm=()=>{
-    return (
-        <div>PokemonsForm</div>
-    );
+import { Field, Form,Formik } from "formik";
+
+const PokemonsForm = () => {
+  return (
+        <Formik 
+            initialValues={{
+                name:'',
+                type:'',
+                hp:'',
+                attack:'',
+                special:'',
+                image:{}
+            }}
+            onSubmit={(values)=>{
+                console.log(values);
+            }}
+        >
+            {({handleSubmit})=>{
+                return(
+                    <Form
+                        onSubmit={handleSubmit}
+                    >
+                        <Field name='name' placeholder='Nombre' className="bg-zinc-800 rounded p-2"/>
+                        <Field name='type' placeholder='Tipo' className="bg-zinc-800 rounded p-2"/>
+                        <Field name='hp' placeholder='Hp' className="bg-zinc-800 rounded p-2"/>
+                        <Field name='attack' placeholder='Ataque' className="bg-zinc-800 rounded p-2"/>
+                        <Field name='special' placeholder='Especial' className="bg-zinc-800 rounded p-2"/>
+                        <button type='submit'>Enviar</button>
+                    </Form>
+                );
+            }}
+        </Formik>
+  );
 };
 
 export default PokemonsForm;
