f2adf529fc4de1428cbaa93275465ec9310e9654
Racha777 <jramoschang@gmail.com>
2022-05-28 12:45:11 -0500

    Funcion HotToast reutilizable

--- src/component/pokemons/PokemonsCard.jsx
+++ src/component/pokemons/PokemonsCard.jsx
@@ -1,45 +1,47 @@
 import { Link } from "react-router-dom";
-import {toast} from "react-hot-toast";
+import { toast } from "react-hot-toast";
 import usePokemons from "../../hooks/usePokemons";
 
 const PokemonsCard = ({ pokemon }) => {
   const { _id, name, type, hp, attack, special, image } = pokemon;
-  const {deletePokemon}=usePokemons();
+  const { deletePokemon } = usePokemons();
 
   const handleDelete = (_id) => {
-    toast((t) =>
-    (
-      <div className="flex flex-col gap-2">
-        <h5 className="text-lg font-medium text-center">¿Deseas eliminarlo?</h5>
-        <div className="flex gap-2">
+    toast((t) => {
+      return (
+        <div className="flex flex-col gap-2">
+          <h5 className="text-lg text-center font-medium">
+            ¿Desea eliminarlo?
+          </h5>
+          <div className="flex gap-2">
           <button
-            className="bg-rose-500 rounded-md text-white font-medium px-3 py-1 cursor-pointer transition-colors hover:bg-rose-600"
-            onClick={() => {
-              deletePokemon(_id);
-              toast.dismiss(t.id);
-            }}
-          >
-            Eliminar
-          </button>
-          <button
-            onClick={() => toast.dismiss(t.id)}
-            className="bg-cyan-300 rounded-md text-black font-medium px-3 py-1 cursor-pointer transition-colors hover:bg-white"
-          >
-            Cancelar
-          </button>
+              className="bg-rose-500 rounded-md text-white font-medium px-3 py-1 transition-colors hover:bg-rose-600"
+              onClick={() => {
+                deletePokemon(_id);
+                toast.dismiss(t.id);
+              }}
+            >
+              Eliminar
+            </button>
+            <button
+              className="bg-cyan-300 rounded-md text-black font-medium px-3 py-1 transition-colors hover:bg-white"
+              onClick={()=>toast.dismiss(t.id)}
+            >
+              Cancelar
+            </button>
+            
+          </div>
         </div>
-      </div>
-    ),
-      {
-        icon: '❗❕❗',
-        style: {
-          borderRadius: '1rem',
-          backgroundColor: '#20232a',
-          color: '#fff',
-          boxShadow: '0 0 0 0.125rem #8888, 0 0 0 0.25rem #8888'
-        },
-      }
-    );
+      );
+    }, {
+      icon: '🗑',
+      style: {
+        backgroundColor: '#20232a',
+        borderRadius: '1rem',
+        boxShadow: '0 0 0 0.125rem #8888, 0 0 0 0.25rem #8888',
+        color: '#fff'
+      },
+    });
   };
 
   return (
@@ -73,7 +75,7 @@ const PokemonsCard = ({ pokemon }) => {
         </Link>
         <button
           className="bg-rose-500 rounded-md text-white font-medium px-3 py-1 transition-colors hover:bg-rose-600"
-          onClick={()=>handleDelete(_id)}
+          onClick={() => handleDelete(_id)}
         >
           Eliminar
         </button>
