c034b811f541d73823eb66507c469f59b0515ff4
Racha777 <jramoschang@gmail.com>
2022-05-30 20:06:35 -0500

    Tarjeta de 0 pokemones y Footer

--- src/layouts/Pokemons/PokemonsLayout.jsx
+++ src/layouts/Pokemons/PokemonsLayout.jsx
@@ -1,19 +1,24 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../../component/sections/Footer";
 import Header from "../../component/sections/Header";
 import usePokemons from "../../hooks/usePokemons";
 
 const PokemonsLayout = () => {
-  const applicationName="PokemonInc";
+  const applicationName = "PokemonInc";
+
+  const credits = {
+    year: new Date().getFullYear(),
+    author: "Racha",
+  };
 
   return (
     <>
-      <Header
-        applicationName={applicationName}
-      />
+      <Header applicationName={applicationName} />
       <main className="pt-16 pb-8">
         <Outlet></Outlet>
       </main>
-      <footer className="mt-auto text-center">Racha</footer>
+
+      <Footer credits={credits} />
     </>
   );
 };
